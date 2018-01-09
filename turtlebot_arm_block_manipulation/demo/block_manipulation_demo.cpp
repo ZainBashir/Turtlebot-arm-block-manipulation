d047cd6da803e8bd46546c311266c939c8ccaee9
ZainBashir <zain_bashir999@hotmail.com>
2018-01-10 00:52:34 +0100

    Comments update

--- turtlebot_arm_block_manipulation/demo/block_manipulation_demo.cpp
+++ turtlebot_arm_block_manipulation/demo/block_manipulation_demo.cpp
@@ -141,54 +141,56 @@ private:
 public:
 
   bool ppcalled = false;
-      geometry_msgs::Pose blockList[20];  // list of the positions of blocks we;ve found
-      geometry_msgs::Pose armUpPose;
+      geometry_msgs::Pose blockList[20];  // List of the positions of blocks we;ve found
+      geometry_msgs::Pose armUpPose; // Pose of the robot in vertical way (used as initial pose)
 
-      ros::Subscriber vsdone_subscriber;
-      ros::Publisher  ppdone_publisher;
+      ros::Subscriber vsdone_subscriber; // Subscriber used to listen to the status message sent by the Visual Servoing module
+      ros::Publisher  ppdone_publisher; // Published for sending completion signal to the Navigation module
 
-    std_msgs::String robot_msg;
-    std::string armMode;
+    std_msgs::String robot_msg; // Variable used to store the status message sent by the robot
+    std::string armMode; // Current status of the arm (initial position, moving, final position)
     
-
+  // Class constructor:
   BlockManipulationAction() : nh_("~"),
     block_detection_action_("block_detection", true),
     interactive_manipulation_action_("interactive_manipulation", true),
     pick_and_place_action_("pick_and_place", true)
   {
 
+    // Subscribe to the topic through which the robot sends the status message
     vsdone_subscriber = nh_.subscribe("/relay/robot_status", 1, &BlockManipulationAction::robotMessage, this);
 
+    // Publish to the topic through which the robot receives pick and place completion message
     ppdone_publisher = nh_.advertise< std_msgs::String >("/arm_status", 1, true);
 
     // Load parameters
-    nh_.param<std::string>("arm_link", arm_link, "/arm_link");
-    nh_.param<double>(gripper_param + "/max_opening", gripper_open, 0.042);
-    nh_.param<double>("grip_tighten", gripper_tighten, -0.0015);
-    nh_.param<double>("z_up", z_up, 0.12);   // amount to lift during move
-    nh_.param<double>("table_height", z_down, 0.01);
-    nh_.param<double>("block_size", block_size, 0.03);  // block size to detect
+    nh_.param<std::string>("arm_link", arm_link, "/arm_link"); // Reference frame for object localization
+    nh_.param<double>(gripper_param + "/max_opening", gripper_open, 0.042); // Gripper open position
+    nh_.param<double>("grip_tighten", gripper_tighten, -0.0015); // Gripper closed position
+    nh_.param<double>("z_up", z_up, 0.12);   // Arm up position
+    nh_.param<double>("table_height", z_down, 0.01); // Table height
+    nh_.param<double>("block_size", block_size, 0.03);  // Block size to detect
     nh_.param<bool>("once", once, false);
     nh_.param<double>("target_x", target_x, .26);    // X target for first block
     nh_.param<double>("target_y", target_y, -.06);   // Y target for first block 
 
     // Initialize goals
-    block_detection_goal_.frame = arm_link;
-    block_detection_goal_.table_height = z_down;
-    block_detection_goal_.block_size = block_size;
+    block_detection_goal_.frame = arm_link; // Set the block detection reference frame to be the arm's base
+    block_detection_goal_.table_height = z_down; // Set table height
+    block_detection_goal_.block_size = block_size; // Set block's size
     
-    pick_and_place_goal_.frame = arm_link;
-    pick_and_place_goal_.z_up = z_up;
-    pick_and_place_goal_.gripper_open = gripper_open;
+    pick_and_place_goal_.frame = arm_link; // Set the pick and place algorithm reference frame to be the arm's base
+    pick_and_place_goal_.z_up = z_up; // Set arm's up position
+    pick_and_place_goal_.gripper_open = gripper_open; // Set gripper opened's position in meters
     //pick_and_place_goal_.gripper_closed = block_size - 0.01 - gripper_tighten;
-    pick_and_place_goal_.gripper_closed = block_size  - gripper_tighten;
-    pick_and_place_goal_.topic = pick_and_place_topic;
+    pick_and_place_goal_.gripper_closed = block_size  - gripper_tighten; // Set gripper closed's position in meters
+    pick_and_place_goal_.topic = pick_and_place_topic; // Set pick and place algorithm's topic
    
     
     ROS_INFO("Gripper settings: closed=%.4f block size=%.4f tighten=%.4f", (float) pick_and_place_goal_.gripper_closed, (float) block_size, (float) gripper_tighten );
     
-    interactive_manipulation_goal_.block_size = block_size;
-    interactive_manipulation_goal_.frame = arm_link;
+    interactive_manipulation_goal_.block_size = block_size; // Send size of the block to the interactive manipulation algorithm
+    interactive_manipulation_goal_.frame = arm_link; // Set the interactive manipulation algorithm to have as reference frame the arm's base
     
     ROS_INFO("Finished initializing, waiting for servers...");
     
@@ -203,22 +205,25 @@ public:
     
   }
 
+    // Callback function that stores the message received from the navigation algorithm:
     void robotMessage(const std_msgs::String& msg){
         robot_msg.data = msg.data;
         }
 
+  // Callback function in charge of executing the block detection algorithm:
   void detectBlocks()
   {
   // Have Block Detection Server detect blocks and callback "addBlocks" when done
     block_detection_action_.sendGoal(block_detection_goal_, boost::bind( &BlockManipulationAction::addBlocks, this, _1, _2));
   }
 
-  
+  // Function in charge of gathering the detected blocks from the detection algorithm and sending them to the interactive manipulation algorithm
   void addBlocks(const actionlib::SimpleClientGoalState& state, const BlockDetectionResultConstPtr& result)
   {
     //ROS_INFO(" Got block detection callback. Adding blocks.");
     geometry_msgs::Pose block;
     
+    //Check if the block detection algorithm has failed or not:
     if (state != actionlib::SimpleClientGoalState::SUCCEEDED)
     {
       ROS_ERROR("  Failed! %s",  state.toString().c_str());
@@ -240,7 +245,7 @@ public:
     blockCount = result->blocks.poses.size();
     blockIndex = 0;
     
-    // Add blocks to Interactive Manipulation Server for Rviz visualization
+    // Add blocks to Interactive Manipulation Server for Rviz visualization and perform pick and place of the detected block:
     //ROS_INFO("Adding blocks...");
     interactive_manipulation_action_.sendGoal(interactive_manipulation_goal_, boost::bind( &BlockManipulationAction::pickAndPlace, this, _1, _2));
 
@@ -248,6 +253,7 @@ public:
 
   }
   
+  // Callback function for performing pick and place of the block that has been detected
   void pickAndPlace(const actionlib::SimpleClientGoalState& state, const InteractiveBlockManipulationResultConstPtr& result)
   {
     if (state != actionlib::SimpleClientGoalState::SUCCEEDED)
@@ -259,6 +265,7 @@ public:
     //ROS_INFO(" Got interactive marker callback.");
     ROS_INFO("Picking and placing...");
 
+    // Set the arm state to moving:
     armMode="m";
 
     //blockList = [];
@@ -267,28 +274,36 @@ public:
     pick_and_place_action_.sendGoal(pick_and_place_goal_, boost::bind( &BlockManipulationAction::finish, this, _1, _2));
   }
   
+  // Callback function activated after the arm has finished putting the cube on the robot
   void finish(const actionlib::SimpleClientGoalState& state, const PickAndPlaceResultConstPtr& result)
   {
+    //Check status of the pick and place algorithm:
     if (state == actionlib::SimpleClientGoalState::SUCCEEDED)
       ROS_INFO(" Pick and place - Succeeded!");
     else
       ROS_INFO(" Pick and place - Failed! %s",  state.toString().c_str());
 
+    // If the "once" parameter is set, stop ROS:
     if (once)
       ros::shutdown();
             
    //moveBlock(armUpPose,armUpPose);
+      
+   // Set arm state to finished mode:
    armMode="i";
 
   }
   
+  // Function to move the arm to a desired pose in the world frame:
   void moveBlock(const geometry_msgs::Pose& start_pose, const geometry_msgs::Pose& end_pose)
   {
+  // Amount (in meters) to bump the result by in the z dimension.
   double bump_size = 0.005;
   
+    // Set arm status to moving
     armMode="m";
+      
     // Return pickup and place poses as the result of the action
-    
     start_pose_bumped = start_pose;
     start_pose_bumped.position.z -= block_size/2.0 - bump_size;
     
@@ -320,6 +335,7 @@ public:
 int main(int argc, char** argv)
 {
 
+  // Initialize pick and place completion message:
   std_msgs::String ppdoneMsg;
   ppdoneMsg.data = "ppdone";
   // initialize node
@@ -334,6 +350,7 @@ int main(int argc, char** argv)
 
     ros::Duration(2).sleep(); // sleep for 2 second
 
+    // Make the arm move to a straight up position:
     manip.armUpPose.position.x = 0.00771822780371;
     manip.armUpPose.position.y = 0.00868746638298;
     manip.armUpPose.position.z = 0.354724377394;
@@ -343,12 +360,16 @@ int main(int argc, char** argv)
     manip.armUpPose.orientation.z = 0;
     manip.moveBlock(manip.armUpPose,manip.armUpPose);
     //manip.blockList = [];
+    
+    // Reset vector of blocks:
     memset(manip.blockList, 0x00, 20*sizeof(int));
 
+  // Run loop at a frequency of 4 Hz:
   ros::Rate r(0.25);
 
   while (ros::ok())
   {
+    // If the arm is not moving already and there hasn't been any pick and place:
     if (manip.armMode!="m" && manip.ppcalled==false) {
 
         std::cout << "  WAITING FOR ROBOT MESSAGE!!!!!!" << std::endl;
@@ -357,13 +378,16 @@ int main(int argc, char** argv)
 
         if (instr == "d") {manip.detectBlocks(); manip.armMode="d";}*/
     
+        // Wait for Visual Servoing completion message
         if (manip.robot_msg.data == "vsdone") {
+            //If message is received, perform block detection and pick and place:
             std::cout << " MESSAGE RECEIVED XDDDDDDDDDDDDDDDDD" << std::endl;
             manip.robot_msg.data = "";
             manip.detectBlocks(); manip.armMode="d";
         }
 
     } else if (manip.armMode=="i" && manip.ppcalled==true) {
+        // If pick and place has been finished, send completion mesage back to the robot:
         std::cout << " Sending pick and place finished message..." << std::endl;
         manip.ppdone_publisher.publish(ppdoneMsg);
     }
